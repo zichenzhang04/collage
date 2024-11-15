42c48a5d92ed72d31e527352847495ae9b1b0c55
Alex Wang <alxswang@umich.edu>
2024-11-15 01:43:36 -0500

    deleted unuseful index import line

--- collage/server/__init__.py
+++ collage/server/__init__.py
@@ -6,4 +6,3 @@ from collage.server.course_page import get_course
 from collage.server.agent import collage_ai_agent
 from collage.server.user_profile import get_registration_info
 from collage.server.course_page import get_course
-from collage.server.index import get_index
